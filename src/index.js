faabaafbe354c781a4c5c7485296df53e676b7ef
Simon Luu <luus2016@gmail.com>
2017-08-30 00:54:30 -0700

    removed alexa code and updated image size

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Alexa from 'alexa-sdk';
 
 import configureStore from './common/store/configureStore';
 import registerServiceWorker from './client/registerServiceWorker';
@@ -9,24 +8,6 @@ import registerServiceWorker from './client/registerServiceWorker';
 import App from './common/components/App';
 
 import './client/styles/index.css';
-
-export default (event, context, callback) => {
-  const alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
-
-const handlers = {
-  'LaunchRequest': function () {
-    this.emit('SayHello"');
-  },
-  'HelloWorldIntent': function () {
-    this.emit('SayHello');
-  },
-  'SayHello': function () {
-    this.emit(':tell', 'Hello World!');
-  }
-};
 
 const store = configureStore();
 
